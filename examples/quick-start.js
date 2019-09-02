49453a5fdd24cf968501e7c47ecf9cb9de1ae120
Akash jobanputra <akash.jobanputra@hotmail.com>
2019-09-02 17:08:14 +0530

    updated quick-start sample to include error details

--- examples/quick-start.js
+++ examples/quick-start.js
@@ -5,7 +5,15 @@ checkIfTypeArray = (param) => {
         throw new Errors.InvalidInputError({
             details: {  // for additional details.
                 message: "the param type for needs to be Array.",
-                param
+                param,
+                error: {        // sample error from SQL
+                    "code": "ER_NO_REFERENCED_ROW_2",
+                    "errno": 1452,
+                    "sqlMessage": "Cannot add or update a child row: a foreign key constraint fails (`devdb`.`userNotificationToken`, CONSTRAINT `UserNotificationFK` FOREIGN KEY (`userId`) REFERENCES `mstUser` (`id`) ON DELETE CASCADE ON UPDATE CASCADE)",
+                    "sqlState": "23000",
+                    "index": 0,
+                    "sql": "INSERT INTO userNotificationToken (userId, token, deviceName, secondsOffset) VALUES (1, 'sampleToken', 'iPhone X', 19800)"
+                  }
             }
         });
     }
