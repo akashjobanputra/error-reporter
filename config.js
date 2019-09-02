98629bfa05817036c0994fee62c561be822b2db8
Akash jobanputra <akash.jobanputra@hotmail.com>
2019-09-02 16:14:24 +0530

    updated config

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    NODE_ENV: 'DEV',
+    NODE_ENV: 'PROD ',
     emailService: 'gmail',
     emailAccountUserName: 'example@gmail.com',
     emailAccountPassword: 'example@password',
-    fromEmail: 'donotreply@example.com'
+    devMailIds: ['dev1@example.com'],
+    adminMailIds: ['admin1@example.com'],
 }
